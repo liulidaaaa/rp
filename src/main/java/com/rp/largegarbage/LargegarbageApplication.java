359c1db1879eddca3feb8b801d9bdd6a81e9f082
liulidaaa <2979284403@qq.com>
2020-06-18 17:35:02 +0800

    第一次上传

---
+++ src/main/java/com/rp/largegarbage/LargegarbageApplication.java
@@ -0,0 +1,13 @@
+package com.rp.largegarbage;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LargegarbageApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LargegarbageApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/rp/largegarbage/LargegarbageApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rp.largegarbage;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LargegarbageApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
