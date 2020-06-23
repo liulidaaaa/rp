fdf75f3e92bf7a68dd9946f2cfb611d6460c1d81
liulida <2979284403@qq.com>
2020-06-23 17:21:32 +0800

    第四次提交

--- src/test/java/com/rp/largegarbage/LargegarbageApplicationTests.java
+++ src/test/java/com/rp/largegarbage/LargegarbageApplicationTests.java
@@ -7,6 +7,9 @@ import com.rp.largegarbage.entity.User;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.serializer.Jackson2JsonRedisSerializer;
+import org.springframework.data.redis.serializer.JdkSerializationRedisSerializer;
 import org.springframework.transaction.annotation.Transactional;
 import java.time.LocalDateTime;
 import java.util.Date;
@@ -20,6 +23,8 @@ class LargegarbageApplicationTests {
 	@Autowired
 	private TokenDao tokenDao;
 
+	@Autowired
+	private RedisTemplate<String, Object> redisTemplate;
 
 	@Transactional
 	@Test
@@ -39,6 +44,68 @@ class LargegarbageApplicationTests {
 		token.setUserId(3);
 		token.setExpireTime(new Date());
 		tokenDao.save(token);
+	}
+	/**
+	 * 添加一个字符串
+	 */
+	@Test
+	public void testSet(){
+		this.redisTemplate.opsForValue().set("key", "bobokaoya...");
+	}
+
+	/**
+	 * 获取一个字符串
+	 */
+	@Test
+	public void testGet(){
+		String value = (String)this.redisTemplate.opsForValue().get("key");
+		System.out.println(value);
+	}
+
+	/**
+	 * 添加Users对象
+	 */
+	@Test
+	public void testSetUesrs(){
+		User users = new User();
+		users.setUserId(1);
+		users.setUsername("1");
+		//重新设置序列化器
+		this.redisTemplate.setValueSerializer(new JdkSerializationRedisSerializer());
+		this.redisTemplate.opsForValue().set("users", users);
+	}
+
+	/**
+	 * 取Users对象
+	 */
+	@Test
+	public void testGetUsers(){
+		//重新设置序列化器
+		this.redisTemplate.setValueSerializer(new JdkSerializationRedisSerializer());
+		User users = (User)this.redisTemplate.opsForValue().get("users");
+		System.out.println(users);
+	}
+
+	/**
+	 * 基于JSON格式存Users对象
+	 */
+	@Test
+	public void testSetUsersUseJSON(){
+		User users = new User();
+		users.setUserId(1);
+		users.setUsername("1");
+		this.redisTemplate.setValueSerializer(new Jackson2JsonRedisSerializer<>(User.class));
+		this.redisTemplate.opsForValue().set("users_json", users);
+	}
+
+	/**
+	 * 基于JSON格式取Users对象
+	 */
+	@Test
+	public void testGetUseJSON(){
+		this.redisTemplate.setValueSerializer(new Jackson2JsonRedisSerializer<>(User.class));
+		User users = (User)this.redisTemplate.opsForValue().get("users_json");
+		System.out.println(users);
 	}
 
 }
